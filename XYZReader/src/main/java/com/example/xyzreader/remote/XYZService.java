568f2381d488bbb89d073edcdd95cc0dd7953dc4
Antony <antony@digitizen.com>
2015-12-17 06:28:15 +0400

    Initial commit

---
+++ XYZReader/src/main/java/com/example/xyzreader/remote/XYZService.java
@@ -0,0 +1,15 @@
+package com.example.xyzreader.remote;
+
+import com.example.xyzreader.model.Article;
+
+import java.util.List;
+
+import retrofit.http.GET;
+/**
+ * Created by Anton on 12/17/2015.
+ */
+public interface XYZService {
+
+    @GET("/u/231329/xyzreader_data/data.json")
+    List<Article> readData();
+}
---
+++ XYZReader/src/main/java/com/example/xyzreader/ui/DynamicHeightImageView.java
@@ -0,0 +1,40 @@
+package com.example.xyzreader.ui;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class DynamicHeightImageView extends ImageView {
+    private float mAspectRatio = 1.5f;
+
+    public DynamicHeightImageView(Context context) {
+        super(context);
+    }
+
+    public DynamicHeightImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DynamicHeightImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public void setAspectRatio(float aspectRatio) {
+        mAspectRatio = aspectRatio;
+        requestLayout();
+    }
+
+    @Override
+    public void setImageBitmap(Bitmap bm) {
+        super.setImageBitmap(bm);
+        requestLayout();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int measuredWidth = getMeasuredWidth();
+        setMeasuredDimension(measuredWidth, (int) (measuredWidth / mAspectRatio));
+    }
+}
